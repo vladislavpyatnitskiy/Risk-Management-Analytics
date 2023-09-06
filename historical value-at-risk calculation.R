85dcf5ffd5e54703ec0515bd445b681cd2fcf9f0
Vladislav Pyatnitskiy <111236801+vladislavpyatnitskiy@users.noreply.github.com>
2023-09-06 20:49:48 +0500

    Update historical value-at-risk calculation.R
    tab part of script

--- historical value-at-risk calculation.R
+++ historical value-at-risk calculation.R
@@ -5,22 +5,22 @@ histVaR <- function(x, y){
   if (nrow(x) < 100) {
     print("Error. Insufficient number of observations for analysis.")
   } else {
-  # Calculate its quantile
-  VaR_y <- 1 - y * 0.01
+    # Calculate its quantile
+    VaR_y <- 1 - y * 0.01
+    
+    # Calculate historical VaR value 
+    x <- apply(x,
+          2,
+          function(col) quantile(col, VaR_y))
+    
+    # Transform into matrix format
+    x <- as.matrix(x)
+    
+    # Put name for column
+    colnames(x) <- c(sprintf("VaR %s", y))
   
-  # Calculate historical VaR value 
-  x <- apply(x,
-        2,
-        function(col) quantile(col, VaR_y))
-  
-  # Transform into matrix format
-  x <- as.matrix(x)
-  
-  # Put name for column
-  colnames(x) <- c(sprintf("VaR %s", y))
-
-  # Display matrix
-  return(x)
+    # Display matrix
+    return(x)
   }
 }
 #Test new function
