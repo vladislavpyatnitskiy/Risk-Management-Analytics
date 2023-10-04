866e6cc571b4ff279a5f97fcf15e59bfe9081e44
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-04 23:33:51 +0500

    Update Historical CVaR.R
    Optimised script:
    
    • Made optional to calculate log returns
    • Reduced excessive scripts
    • Assigned variable to x[,n] to decrease chance of error
    • Merged steps of selecting 5% worst returns, calculating their means and adding to list

--- Expected Shortfall (CVaR)/Historical CVaR.R
+++ Expected Shortfall (CVaR)/Historical CVaR.R
@@ -1,55 +1,33 @@
 # Function to get Expected Shortfall via historical method
-histCVaR <- function(x, y){
+histCVaR <- function(x, y, lg = T){
   
   # Check whether there are less than 100 observations
-  if (nrow(x) < 100) {
-    
-    # Because with sample of less than 100 observations CVaR is irrelevant
-    print("Error. Insufficient number of observations for analysis.")
-    
-    # When there are 100 and more observations
-  } else {
-    
-    # Calculate its quantile
-    CVaR_y <- 1 - y * 0.01
-    
-    # Calculate log returns and remove NA
-    x <- diff(log(x))[-1,]
-    
-    # Get column names from data set
-    names_for_cvar <- colnames(x)
-    
+  if (nrow(x) < 100) { print("Error. Insufficient number of observations.") } 
+  
+  # Otherwise calculate logs & remove NA if necessary & create list for CVaR
+  else { if (isTRUE(lg)) { x <- diff(log(x))[-1,] } 
+
     # Create variable to contain values for CVaR
     list_for_cvar <- NULL
     
-    # For each column in dataset
-    for (n in 1:ncol(x)){
+    # For each column in dataset assign variable to each column
+    for (n in 1:ncol(x)){ security <- x[,n]
       
       # Sort values ascendingly
-      testcvar <- x[,n][order(x[,n]),]
+      testcvar <- security[order(security),]
       
-      # Select ones that in 5% worst observations
-      number_for_cvar <- testcvar[1:(CVaR_y * nrow(testcvar)),]
-      
-      # Find their mean
-      cvar_value <- mean(number_for_cvar)
-      
-      # Add the value in list
-      list_for_cvar <- c(list_for_cvar, cvar_value)
-    }
-    
-    # Transform into matrix format
-    list_for_cvar <- as.matrix(list_for_cvar)
-    
+      # Select 5% worst observations, find their means and add to list
+      list_for_cvar <- rbind(list_for_cvar,
+                             mean(testcvar[1:((1 - y * 0.01) *
+                                                nrow(testcvar)),])) }
     # Put name for column
-    colnames(list_for_cvar) <- c(sprintf("CVaR %s", y))
+    colnames(list_for_cvar) <- sprintf("CVaR %s%%", y)
     
-    # Give them names frm data set
-    rownames(list_for_cvar) <- names_for_cvar
+    # Give them names from data set
+    rownames(list_for_cvar) <- colnames(x)
     
     # Display matrix
-    return(list_for_cvar)
-  }
+    return(list_for_cvar) }
 }
 # Test
 histCVaR(stock_data, 95)
