af2b7eb29e0f208bd38da96a106178c2d8f54353
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-30 13:03:55 +0500

    Update VaR via Duration.R
    Optimised script:
    
    • Reduce excessive lines & brackets
    • Improved equation for various payment frequency scenarios

--- Value-at-Risk/VaR via Duration.R
+++ Value-at-Risk/VaR via Duration.R
@@ -1,19 +1,17 @@
 # VaR
 VaR.D <- function(P, C, r, y, f = 1, s = 1, VaR = 95, PV = NULL, p = NULL){
   
-  B <- P*(C*(1/r*(1-1/(1+(1/(f/r)))^(y*f)))+1/(1+(1/(f/r)))^(y*f)) # Bond Price
+  B <- P*(C/f*(1/(r/f)*(1-1/(1+r/f)^(y*f)))+1/(1+r/f)^(y*f)) # Bond Price
   
-  P. <- P * (1 + C / f) / (1 + r) ^ (y * f) # Principle Part
+  P. <- P * (1 + C / f) / (1 + r / f) ^ (y * f) # Principle Part
   
-  for (n in 1:(y - 1)){ PV <- cbind(PV, (C * P/f )/(1 + r )^n) # PV of coupons
+  for (n in 1:(y*f-1)){ PV <- cbind(PV, ((C*P)/f)/(1 + r/f)^(n*f)) # Coupon PV
   
-  p <- cbind(p, n * PV[n]) } # Coupon part for numerator
+    p <- cbind(p, n * PV[n]) } # Coupon part for numerator
   
-  D <- (sum(p[seq(y - 1)]) + P. * y) / (P. + sum(PV[seq(y - 1)])) # Duration
+  D <- (sum(p[seq(y*f - 1)]) + P.*y*f)/(P. + sum(PV[seq(y*f - 1)])) # Duration
   
-  VaR.d <- B * (qnorm(1 - VaR * 0.01) * (D/(1 + (r - s * 0.01)/f))) * r # VaR
-  
-  return(VaR.d) # Display sentence
+  B * qnorm(1 - VaR * 0.01) * (D / (1 + (r - s * 0.01) / f)) * (r / f) # VaR
 }
 # Test
 VaR.D(1000, 0.1, 0.05, 3, 1, 1)
