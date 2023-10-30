941ddb6ce0e2df4c4d75ffb922930b8f046e1e43
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-10-30 12:48:58 +0500

    Update VaR via Monte Carlo.R
    Optimised script:
    
    • Renamed variables
    • Reduced excessive lines
    • Combined operations

--- Value-at-Risk/VaR via Monte Carlo.R
+++ Value-at-Risk/VaR via Monte Carlo.R
@@ -1,60 +1,30 @@
 # Libraries
-lapply(c("quantmod",
-         "ggplot2",
-         "data.table",
-         "timeSeries"),
-       require,
-       character.only = TRUE)
+lapply(c("quantmod", "ggplot2", "data.table", "timeSeries"),
+       require, character.only = T)
 
 # Monte Function
-monte_carlo_for_var <- function(c, ndays, n, VaR_for_monte = 95){
+monte.carlo.var <- function(c, ndays, n, VaR = 95){
   
-  # Set list to store values
-  list_var_mc <- NULL
-  
-  # Set title for plot
-  title_for_var <- colnames(c)
+  l.var <- NULL # Set list to store values
   
   # For each column in data set
-  for (b in 1:ncol(c)){
+  for (b in 1:ncol(c)){ lrtn <- as.numeric(c[,b] / lag(c[,b]))
     
-    # Define name for column variable
-    security <- c[,b]
+    lrtn[1] <- 1 # Define first value in column as 1
     
-    # Calculate return
-    lrtn <- as.numeric(security / lag(security))
-    
-    # Define first value in column as 1
-    lrtn[1] <- 1
-    
-    # Calculate various scenarios of Stock Performance
-    set.seed(0)
-    
-    # Mimic Historical Performance using log returns
-    paths <- replicate(n, expr = round(sample(lrtn,ndays,replace = TRUE),2))
-    
-    # Put values into list and calculate cumulative sums
-    paths <- apply(paths, 2, cumprod)
-    
-    # Transform it into Time Series
-    paths <- data.table(paths)
+    set.seed(0) # Calculate various scenarios of Stock Performance
+    paths <- replicate(n, expr = round(sample(lrtn, ndays, replace = T), 2))
+    paths <- data.table(apply(paths, 2, cumprod)) # Calculate cumulative sums
     paths$days <- 1:nrow(paths)
-    paths <- melt(paths,
-                  id.vars = "days")
+    paths <- melt(paths, id.vars = "days")
     
-    # Calculate VaR and add to list
-    list_var_mc <- rbind(list_var_mc, 
-                         quantile(((paths$value[paths$days == ndays] - 1) *
-                                     100), 1 - VaR_for_monte * 0.01) / ndays)
-  }
-  # Give row name
-  rownames(list_var_mc) <- title_for_var
+    l.var <- rbind(l.var, quantile(((paths$value[paths$days==ndays]-1)*100),
+                                   1 - VaR * 0.01))} # Add VaR to list
+  # Give row and column names
+  rownames(l.var) <- colnames(c)
+  colnames(l.var) <- "VaR MC"
   
-  # Give column name
-  colnames(list_var_mc) <- "VaR MC"
-  
-  # Display values
-  return(list_var_mc)
+  return(l.var) # Display values
 }
 # Test
-monte_carlo_for_var(portfolioReturns, 252, 100)
+monte.carlo.var(portfolioReturns, 252, 100)
