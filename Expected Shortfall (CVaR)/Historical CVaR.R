19bb7f1ee48f510de47edc303e741f2a98f2f0f9
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-02 14:13:02 +0500

    Update Historical CVaR.R
    Renamed variables & edited comments

--- Expected Shortfall (CVaR)/Historical CVaR.R
+++ Expected Shortfall (CVaR)/Historical CVaR.R
@@ -1,33 +1,25 @@
-# Function to get Expected Shortfall via historical method
-histCVaR <- function(x, y, lg = T){
+# Expected Shortfall via historical method
+histCVaR <- function(x, CVaR, lg = T){ 
   
   # Check whether there are less than 100 observations
   if (nrow(x) < 100) { print("Error. Insufficient number of observations.") } 
   
-  # Otherwise calculate logs & remove NA if necessary & create list for CVaR
-  else { if (isTRUE(lg)) { x <- diff(log(x))[-1,] } 
-
-    # Create variable to contain values for CVaR
-    list_for_cvar <- NULL
+  if (isTRUE(lg)) { x <- diff(log(x))[-1,] } # Logs in & NA off if necessary
     
-    # For each column in dataset assign variable to each column
-    for (n in 1:ncol(x)){ security <- x[,n]
-      
-      # Sort values ascendingly
-      testcvar <- security[order(security),]
+    l <- NULL # Create variable to contain values for CVaR
+    
+    for (n in 1:ncol(x)){ s <- x[,n] # Assign variable to each column
+    
+      v <- s[order(s),] # Sort values ascendingly
       
       # Select 5% worst observations, find their means and add to list
-      list_for_cvar <- rbind(list_for_cvar,
-                             mean(testcvar[1:((1 - y * 0.01) *
-                                                nrow(testcvar)),])) }
-    # Put name for column
-    colnames(list_for_cvar) <- sprintf("CVaR %s%%", y)
+      l <- rbind(l, mean(v[1:((1 - CVaR * 0.01) * nrow(v)),])) }
     
-    # Give them names from data set
-    rownames(list_for_cvar) <- colnames(x)
+    colnames(l) <- sprintf("CVaR %s%%", CVaR) # Put name for column
     
-    # Display matrix
-    return(list_for_cvar) }
+    rownames(l) <- colnames(x) # Give them names from data set
+    
+    return(l) # Display matrix
 }
 # Test
-histCVaR(stock_data, 95)
+histCVaR(stock_data, 95, T)
