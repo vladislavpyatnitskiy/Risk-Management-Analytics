9a27a03dd9ce1d6d5413a96143547cdb8cd01a33
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-06 17:18:49 +0500

    Update Rachev Ratio.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined operations
    • Edited comments
    • Renamed variables

--- Rachev Ratio.R
+++ Rachev Ratio.R
@@ -1,5 +1,5 @@
 # Function to calculate Rachev Ratio via historical method
-Rachev.ratio <- function(x, y, lg = T){
+Rachev.ratio <- function(x, VaR, lg = T){
   
   # Check whether there are less than 100 observations
   if (nrow(x) < 100) { print("Error. Insufficient number of observations.") } 
@@ -7,37 +7,22 @@ Rachev.ratio <- function(x, y, lg = T){
   # Otherwise calculate logs & remove NA if necessary & create list for CVaR
   else { if (isTRUE(lg)) { x <- diff(log(x))[-1,] } 
     
-    # Create variable to contain values for positive and negative CVaRs
-    list_for_cvar_minus <- NULL
-    list_for_cvar_max <- NULL
+    l.minus <- NULL # list with negative CVaR values
+    l.max <- NULL # list with positive CVaR values
     
-    # For each column in dataset assign variable to each column
-    for (n in 1:ncol(x)){ security <- x[,n]
-    
-      # Sort values ascendingly
-      testcvar <- security[order(security),]
+    # For each column in dataset assign variable & Sort ascendingly
+    for (n in 1:ncol(x)){ es <- x[,n][order(x[,n]),]
       
-      # Select 5% worst observations, find their means and add to list
-      list_for_cvar_minus <- rbind(list_for_cvar_minus,
-                                   mean(testcvar[1:((1 - y * 0.01) *
-                                                      nrow(testcvar)),])) 
-      
-      # Select 5% best observations, find their means and add to list
-      list_for_cvar_max <- rbind(list_for_cvar_max,
-                                 mean(testcvar[((y * 0.01) *nrow(testcvar)):
-                                                 nrow(testcvar),]))
-    }
-    # Calculate Rachev Ratio
-    rr.coef <- round(list_for_cvar_max / (-list_for_cvar_minus), 3)
+      # Select 5% worst and 5% best obs, find their means and add to list
+      l.minus <- rbind(l.minus, mean(es[1:((1 - VaR * .01) * nrow(es)),])) 
+      l.max <- rbind(l.max, mean(es[(VaR * .01 * nrow(es)):nrow(es),])) }
     
-    # Put name for column
-    colnames(rr.coef) <- sprintf("Rachev Ratio at %s%%", y)
+    rr.coef <- round(l.max / -l.minus, 3) # Calculate Rachev Ratio
     
-    # Give them names from data set
-    rownames(rr.coef) <- colnames(x)
+    colnames(rr.coef) <- sprintf("Rachev Ratio at %s%%", VaR) # Name for column
     
-    # Display matrix
-    return(rr.coef) }
+    rownames(rr.coef) <- colnames(x) # Give them names from data set
+    
+    return(rr.coef) } # Display matrix
 }
-# Test
-Rachev.ratio(stock_data, 95)
+Rachev.ratio(stock_data, 95) # Test
