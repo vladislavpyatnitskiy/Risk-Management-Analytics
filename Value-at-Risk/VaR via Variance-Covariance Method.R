b6004aff1e714453dcec63e435dd1b8b3ba7e8ef
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2023-11-14 23:44:42 +0500

    Update VaR via Variance-Covariance Method.R
    Optimised script:
    
    • Reduced excessive lines
    • Edited comments
    • Renamed variables

--- Value-at-Risk/VaR via Variance-Covariance Method.R
+++ Value-at-Risk/VaR via Variance-Covariance Method.R
@@ -1,25 +1,21 @@
 # Function to calculate VaR via Variance Covariance Method
-var_vc <- function(x, y = 95, lg = T){
+VaR.VC <- function(x, VaR = 95, lg = T){
   
   # Check whether there are less than 100 observations
   if (nrow(x) < 100) { print("Insufficient number of observations.") } else {
-  
+    
     if (isTRUE(lg)) { x <- diff(log(x))[-1,] } # Log returns and remove NA
     
-    vs_VaR <- apply(x, 2, function(x) c(mean(x), sd(x))) # Means and SDs
+    v <- apply(x,2,function(x) c(mean(x), sd(x))) # Means & Standard Deviations
     
-    var_vc <- NULL # Set up list to contain future values
+    l <- NULL # Set up list to contain future values
     
-    for (n in 1:ncol(x)){ # For each asset
-      
-      # Calculate VaR using standard norm probs and join to list
-      var_vc <- rbind(var_vc, vs_VaR[1,n] + qnorm(1 - y * 0.01) * vs_VaR[2,n])}
+    # Calculate VaR using standard norm probs and join to list
+    for (n in 1:ncol(x)){ l <- rbind(l, v[1,n] + qnorm(1 - VaR*.01) * v[2,n])}
     
-    rownames(var_vc) <- colnames(x) # Return names to assets
-    colnames(var_vc) <- sprintf("VaR VC %s%%", y) } # Name parameter
+    rownames(l) <- colnames(x) # Return names to assets
+    colnames(l) <- sprintf("VaR VC %s%%", VaR) } # Name parameter
   
-  # Display values
-  return(var_vc)
+  return(l) # Display values
 }
-# Test
-var_vc(stock_data, 95)
+VaR.VC(stock_data, 95) # Test
