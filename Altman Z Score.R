6255ff4ea83312918c38470df2474cc25c018583
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-01-23 18:37:49 +0500

    Update Altman Z Score.R
    Optimised script:
    
    • Reduced excessive lines
    • Combined AS.NUMERIC function with the one reducing commas in data

--- Altman Z Score.R
+++ Altman Z Score.R
@@ -1,9 +1,9 @@
 library("rvest") # Library
 
 altmanz.score <- function(x){ l <- NULL # Function to get Altman Z Score
-  
+
   for (n in 1:length(x)){ a <- x[n]
-  
+    
     bs <- sprintf("https://finance.yahoo.com/quote/%s/balance-sheet?p=%s",a,a)
     is <- sprintf("https://finance.yahoo.com/quote/%s/financials?p=%s",a,a)
     re <- sprintf("https://finance.yahoo.com/quote/%s/key-statistics?p=%s",a,a)
@@ -26,54 +26,52 @@ altmanz.score <- function(x){ l <- NULL # Function to get Altman Z Score
     
     if (isFALSE(any(y=="Working Capital"))){ # If Working Capital is off
       
-      w <- cbind(p[grep("Sector", p) + 1], "NA", "NA")
-      
-      colnames(w) <- c("Sector" ,"Value", "Zone")
-      
-      l <- rbind.data.frame(l, w) # Add Sector with NA
-      
-      } else { b <- w[grep("Payout Ratio ", w) + 1] # Payout Ratio
-          
-      b <- as.numeric(read.fwf(textConnection(b), widths = c(nchar(b) - 1, 1),
-                                   colClasses = "character")[, 1]) / 100
-      c <- NULL
-      h <- NULL
-          
-      k <- c("Total Assets", "Total Liabilities Net Minority Interest",
-             "Total Equity Gross Minority Interest", "Working Capital")
-          
-      r <- c("EBIT", "Total Revenue", "Net Income Common Stockholders")
-          
-      for (m in 1:length(r)){ c <- rbind(c, u[grep(r[m], u) + 1][1]) }
-      for (m in 1:length(k)){ h <- rbind(h, y[grep(k[m], y) + 1][1]) }
-          
-      c <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", c)) # Reduce commas
-      h <- gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", h)) # Reduce commas
-          
-      A <- as.numeric(h[4])/as.numeric(h[1])  # Working Capital/Total Assets
-      B <- (1-b)*as.numeric(c[3])/as.numeric(h[1]) # Retention/Total Assets  
-      C <- as.numeric(c[1]) / as.numeric(h[1]) # EBIT / Total Assets
-      D <- as.numeric(h[3]) / as.numeric(h[2]) # Equity / Total Liabilities
-      E <- as.numeric(c[2]) / as.numeric(h[1]) # Total Revenue / Total Assets
-      
-      if (isTRUE(p[grep("Sector", p) + 1] == "Technology")){ 
-        
-        a.r = round(6.56 * A + 3.26 * B + 6.72 * C + 1.05 * D, 2)
-        
-      } else { a.r = round(1.2 * A + 1.4 * B + 3.3 * C + .6 * D + E, 2) }
-      
-      if (a.r > 2.6){ Z <- "Safe" } else if (a.r < 2.6 && a.r > 1.1){
-        
-        Z <- "Grey" } else { Z <- "Distress" } # Assign Health parameter
-      
-      w <- cbind(p[grep("Sector", p) + 1], a.r, Z)
+      w <- cbind(p[grep("Sector", p) + 1], "NA", "NA") # ratios unavailable
       
       colnames(w) <- c("Sector" ,"Value", "Zone") 
       
-      l <- rbind.data.frame(l, w) } } # Add to data frame
-
-  rownames(l) <- x # Assign row names
+      l <- rbind.data.frame(l, w) } else { b <- w[grep("Payout Ratio ", w) + 1] 
     
+    b <- as.numeric(read.fwf(textConnection(b), widths = c(nchar(b) - 1, 1),
+                             colClasses = "character")[, 1]) / 100
+    c <- NULL
+    h <- NULL
+    
+    k <- c("Total Assets", "Total Liabilities Net Minority Interest",
+           "Total Equity Gross Minority Interest", "Working Capital")
+    
+    r <- c("EBIT", "Total Revenue", "Net Income Common Stockholders")
+    
+    for (m in 1:length(r)){ c <- rbind(c, u[grep(r[m], u) + 1][1]) }
+    for (m in 1:length(k)){ h <- rbind(h, y[grep(k[m], y) + 1][1]) }
+    
+    c <- as.numeric(gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", c)))
+    h <- as.numeric(gsub(",", "", gsub("([a-zA-Z]),", "\\1 ", h)))
+    
+    A <- h[4] / h[1]  # Working Capital/Total Assets
+    B <- (1 - b) * c[3] / h[1] # Retention/Total Assets  
+    C <- c[1] / h[1] # EBIT / Total Assets
+    D <- h[3] / h[2] # Equity / Total Liabilities
+    E <- c[2] / h[1] # Total Revenue / Total Assets
+    
+    if (isTRUE(p[grep("Sector", p) + 1] == "Technology")){ 
+      
+      a.r = round(6.56 * A + 3.26 * B + 6.72 * C + 1.05 * D, 2)
+      
+    } else { a.r = round(1.2 * A + 1.4 * B + 3.3 * C + .6 * D + E, 2) }
+    
+    if (a.r > 2.6){ Z <- "Safe" } else if (a.r < 2.6 && a.r > 1.1){
+      
+      Z <- "Grey" } else { Z <- "Distress" } # Assign Health parameter
+    
+    w <- cbind(p[grep("Sector", p) + 1], a.r, Z)
+    
+    colnames(w) <- c("Sector" ,"Value", "Zone") 
+    
+    l <- rbind.data.frame(l, w) } } # Add to data frame
+    
+  rownames(l) <- x # Assign row names
+  
   l # Display
 }
 altmanz.score(c("C", "GOOGL", "MU")) # Test
