ebd8f9481cb709e0fe51dd8a5e90b365e9b90a75
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-08-04 16:19:50 +0500

    Update Rachev Ratio.R
    Optimised script:
    
    • Added option to calculate Rachev Ratio for several CVaR parameters
    • Reduced excessive lines

--- Expected Shortfall (CVaR)/Rachev Ratio.R
+++ Expected Shortfall (CVaR)/Rachev Ratio.R
@@ -1,28 +1,24 @@
-# Function to calculate Rachev Ratio via historical method
-Rachev.ratio <- function(x, VaR, lg = T){
+Rachev.ratio <- function(x, VaR, lg = T){ # Rachev Ratio via historical method
   
-  # Check whether there are less than 100 observations
   if (nrow(x) < 100) { print("Error. Insufficient number of observations.") } 
   
-  # Otherwise calculate logs & remove NA if necessary & create list for CVaR
-  else { if (isTRUE(lg)) { x <- diff(log(x))[-1,] } 
+    else { if (isTRUE(lg)) { x <- diff(log(x))[-1,] } # Make log if needed
     
-    l.minus <- NULL # list with negative CVaR values
-    l.max <- NULL # list with positive CVaR values
+    L <- NULL
     
-    # For each column in dataset assign variable & Sort ascendingly
-    for (n in 1:ncol(x)){ es <- x[,n][order(x[,n]),]
+    for (m in 1:length(VaR)){ l <- NULL # Sort in ascending way
+    
+      # Calculate mean of both best and worst 5% observations and divide
+      for (n in 1:ncol(x)){ es <- x[,n][order(x[,n]),]
       
-      # Select 5% worst and 5% best obs, find their means and add to list
-      l.minus <- rbind(l.minus, mean(es[1:((1 - VaR * .01) * nrow(es)),])) 
-      l.max <- rbind(l.max, mean(es[(VaR * .01 * nrow(es)):nrow(es),])) }
-    
-    rr.coef <- round(l.max / -l.minus, 3) # Calculate Rachev Ratio
-    
-    colnames(rr.coef) <- sprintf("Rachev Ratio at %s%%", VaR) # Name for column
-    
-    rownames(rr.coef) <- colnames(x) # Give them names from data set
-    
-    return(rr.coef) } # Display matrix
+        l <- rbind(l, round(mean(es[(VaR[m] * .01 * nrow(es)):nrow(es),]) /
+                              -mean(es[1:((1 - VaR[m]*.01)*nrow(es)),]), 3)) } 
+      
+      colnames(l) <- sprintf("Rachev Ratio at %s%%", VaR[m]) # Rachev Ratio %
+      rownames(l) <- colnames(x) # Assign tickers
+      
+      L <- cbind(L, l) } # Join
+  
+  L } # Display data frame 
 }
-Rachev.ratio(stock_data, 95) # Test
+Rachev.ratio(stock_data, c(95, 97.5, 99)) # Test
