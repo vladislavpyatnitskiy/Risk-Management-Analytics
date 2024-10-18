cdd45b7d8b87883fb6643c31e0703582b8bc952d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-10-18 14:25:30 +0500

    Update VaR via Variance-Covariance Method.R
    Fixed issue when number of rows is less than 100:
    
    • Issue led to incorrect display of warning
    • Substituted PRINT function to MESSAGE

--- Value-at-Risk/VaR via Variance-Covariance Method.R
+++ Value-at-Risk/VaR via Variance-Covariance Method.R
@@ -1,8 +1,7 @@
-# Function to calculate VaR via Variance Covariance Method
-VaR.VC <- function(x, VaR = 95, lg = T){
+VaR.VC <- function(x, VaR = 95, lg = T){ # VaR via Variance Covariance Method
   
   # Check whether there are less than 100 observations
-  if (nrow(x) < 100) { print("Insufficient number of observations.") } else {
+  if (nrow(x) < 100) { message("Insufficient number of observations.") } else {
     
     if (isTRUE(lg)) { x <- diff(log(x))[-1,] } # Log returns and remove NA
     
@@ -14,8 +13,8 @@ VaR.VC <- function(x, VaR = 95, lg = T){
     for (n in 1:ncol(x)){ l <- rbind(l, v[1,n] + qnorm(1 - VaR*.01) * v[2,n])}
     
     rownames(l) <- colnames(x) # Return names to assets
-    colnames(l) <- sprintf("VaR VC %s%%", VaR) } # Name parameter
+    colnames(l) <- sprintf("VaR VC %s%%", VaR) # Name parameter
   
-  return(l) # Display values
+    return(l) } # Display values
 }
 VaR.VC(stock_data, 95) # Test
