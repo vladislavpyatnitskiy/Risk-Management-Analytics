33bf56fc29ef6810962761062cc4487d06bd4238
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-10-19 11:41:14 +0500

    Update Historical CVaR.R
    Optimised script:
    
    • Fixed issue when data frame has less than 100 rows
    • Substituted PRINT with MESSAGE function

--- Expected Shortfall (CVaR)/Historical CVaR.R
+++ Expected Shortfall (CVaR)/Historical CVaR.R
@@ -1,25 +1,23 @@
-# Expected Shortfall via historical method
-histCVaR <- function(x, CVaR, lg = T){ 
+histCVaR <- function(x, CVaR, lg=T){ # Expected Shortfall via historical method
   
   # Check whether there are less than 100 observations
-  if (nrow(x) < 100) { print("Error. Insufficient number of observations.") } 
+  if (nrow(x) < 100){ message("Error. Insufficient number of observations.") } 
   
-  if (isTRUE(lg)) { x <- diff(log(x))[-1,] } # Logs in & NA off if necessary
+  else { if (isTRUE(lg)) { x <- diff(log(x))[-1,] } 
     
     l <- NULL # Create variable to contain values for CVaR
     
     for (n in 1:ncol(x)){ s <- x[,n] # Assign variable to each column
     
-      v <- s[order(s),] # Sort values ascendingly
+      v <- s[order(s),] # Sort values in ascending way
       
       # Select 5% worst observations, find their means and add to list
       l <- rbind(l, mean(v[1:((1 - CVaR * 0.01) * nrow(v)),])) }
-    
+      
     colnames(l) <- sprintf("CVaR %s%%", CVaR) # Put name for column
     
     rownames(l) <- colnames(x) # Give them names from data set
     
-    return(l) # Display matrix
+    return(l) } # Display matrix
 }
-# Test
-histCVaR(stock_data, 95, T)
+histCVaR(stock_data, 95, T) # Test
