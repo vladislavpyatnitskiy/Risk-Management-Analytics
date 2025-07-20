0d81c2738a603fde9ff98214959a8da5e0cc993d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-20 12:53:19 +0500

    Update historical value-at-risk calculation.R
    Optimised script:
    
    • Eliminated unnecessary ELSE statement and curly brackets

--- Value-at-Risk/historical value-at-risk calculation.R
+++ Value-at-Risk/historical value-at-risk calculation.R
@@ -1,15 +1,15 @@
-histVaR <- function(x, VaR, lg = T){ # Calculate VaR via Historical Method
+histVaR <- function(x, VaR, lg = T){ # Calculate Historical VaR
   
   # Check whether there are less than 100 observations
-  if (nrow(x) < 100) { message("Insufficient number of observations.") } else {
+  if (nrow(x) < 100) return(message("Insufficient number of observations."))
     
-    if (isTRUE(lg)) { x <- diff(log(x))[-1,] } # log returns and remove NA
+  if (lg) x = diff(log(x))[-1,]  # log returns and remove NA
     
-    # Calculate historical VaR value and transform into matrix format
-    x <- as.matrix(apply(x, 2, function(col) quantile(col, 1 - VaR * 0.01)))
+  # Calculate historical VaR value and transform into matrix format
+  x <- as.matrix(apply(x, 2, function(col) quantile(col, 1 - VaR * 0.01)))
     
-    colnames(x) <- c(sprintf("VaR %s%%", VaR)) # Put name for column
+  colnames(x) <- c(sprintf("VaR %s%%", VaR)) # Put name for column
     
-    return(x) } # Display matrix
+  return(x) # Display matrix
 }
 histVaR(stock_data, 95) # Test
