68a05468636587c481249e21e136969e2b1423f4
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-21 11:19:37 +0500

    Update Historical CVaR.R
    Optimised script:
    
    • Reduced unnecessary ELSE statement and curly brackets

--- Expected Shortfall (CVaR)/Historical CVaR.R
+++ Expected Shortfall (CVaR)/Historical CVaR.R
@@ -1,23 +1,23 @@
 histCVaR <- function(x, CVaR, lg=T){ # Expected Shortfall via historical method
   
   # Check whether there are less than 100 observations
-  if (nrow(x) < 100){ message("Error. Insufficient number of observations.") } 
+  if (nrow(x) < 100) return(message("Insufficient number of observations."))
   
-  else { if (isTRUE(lg)) { x <- diff(log(x))[-1,] } 
+  if (lg) x = diff(log(x))[-1,] # log if necessary
     
-    l <- NULL # Create variable to contain values for CVaR
+  l <- NULL # Create variable to contain values for CVaR
     
-    for (n in 1:ncol(x)){ s <- x[,n] # Assign variable to each column
+  for (n in 1:ncol(x)){ s <- x[,n] # Assign variable to each column
     
-      v <- s[order(s),] # Sort values in ascending way
-      
-      # Select 5% worst observations, find their means and add to list
-      l <- rbind(l, mean(v[1:((1 - CVaR * 0.01) * nrow(v)),])) }
-      
-    colnames(l) <- sprintf("CVaR %s%%", CVaR) # Put name for column
+    v <- s[order(s),] # Sort values in ascending way
     
-    rownames(l) <- colnames(x) # Give them names from data set
+    # Select 5% worst observations, find their means and add to list
+    l <- rbind(l, mean(v[1:((1 - CVaR * 0.01) * nrow(v)),])) }
     
-    return(l) } # Display matrix
+  colnames(l) <- sprintf("CVaR %s%%", CVaR) # Put name for column
+    
+  rownames(l) <- colnames(x) # Give them names from data set
+    
+  return(l) # Display matrix
 }
 histCVaR(stock_data, 95, T) # Test
