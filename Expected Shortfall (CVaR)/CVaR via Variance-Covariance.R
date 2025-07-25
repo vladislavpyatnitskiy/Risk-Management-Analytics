51c201211abfa3987cdd321c5fdb37ab3dd44c9d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-25 10:50:55 +0500

    Update CVaR via Variance-Covariance.R
    Optimised script:
    
    • Reduced ELSE statement and excessive lines

--- Expected Shortfall (CVaR)/CVaR via Variance-Covariance.R
+++ Expected Shortfall (CVaR)/CVaR via Variance-Covariance.R
@@ -1,21 +1,21 @@
 CVaR.VC <- function(x, es = 95, lg = T){
   
   # Check whether there are less than 100 observations
-  if (nrow(x) < 100) { message("Insufficient number of observations.") } else {
+  if (nrow(x) < 100) return(message("Insufficient number of observations."))
     
-    if (isTRUE(lg)) { x <- diff(log(x))[-1,] } # Log returns and remove NA
+  if (lg) x <- diff(log(x))[-1,] # Log returns and remove NA
     
-    v <- apply(x,2,function(x) c(mean(x), sd(x))) # Means & Standard Deviations
+  v <- apply(x,2,function(x) c(mean(x), sd(x))) # Means & Standard Deviations
     
-    l <- NULL # Set up list to contain future values
+  l <- NULL # Set up list to contain future values
     
-    # Calculate CVaR using standard norm probs and join to list
-    for (n in 1:ncol(x)){ l <- rbind(l, v[1,n] - dnorm(qnorm(es * .01)) /
-                                       (1 - es * .01) * v[2,n])}
+  # Calculate CVaR using standard norm probs and join to list
+  for (n in 1:ncol(x)){ l <- rbind(l, v[1,n] - dnorm(qnorm(es * .01)) /
+                                     (1 - es * .01) * v[2,n])}
     
-    rownames(l) <- colnames(x) # Return names to assets
-    colnames(l) <- sprintf("ES VC %s%%", es)  # Name parameter
-  
-    return(l) } # Display values
+  rownames(l) <- colnames(x) # Return names to assets
+  colnames(l) <- sprintf("ES VC %s%%", es)  # Name parameter
+    
+  return(l) # Display values
 }
 CVaR.VC(stock_data, 95) # Test
