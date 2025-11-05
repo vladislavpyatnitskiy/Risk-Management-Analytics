9e6cfe383a4e9b9b7ada8f1d49b03494cbadd507
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-11-05 11:41:49 +0500

    Refactor Monte Carlo simulation function
    Refactor Monte Carlo simulation function to improve structure and readability. Consolidate plotting and summary calculations.

--- Monte Carlo Simulation/Monte Carlo Method.R
+++ Monte Carlo Simulation/Monte Carlo Method.R
@@ -2,40 +2,57 @@ lapply(c("quantmod", "ggplot2", "data.table", "timeSeries"),
        require, character.only = T) # Libraries
 
 monte.carlo <- function(x, ndays, n, yahoo = T){ # Monte Carlo Simulation
-  
-  if (isTRUE(yahoo)){ P <- NULL # When Data from Yahoo! Finance needed
+
+  if (yahoo){ P <- NULL # When Data from Yahoo! Finance needed
   
     for (A in x){ P <- cbind(P, getSymbols(A,src="yahoo",auto.assign=F)[,4]) }
-  
+    
     P <- P[apply(P, 1, function(x) all(!is.na(x))),] # Reduce NA
     
     colnames(P) <- x } else { P <- x } # Assign columns
+    
+  if (!is.timeSeries(P)){ P <- as.timeSeries(P) }
   
-  c <- as.timeSeries(P) # Make data Time Series
+  L <- NULL
+  Av <- NULL
+  Plots <- NULL
   
-  r <- as.numeric(c / lag(c)) # Calculate returns
-  r[1] <- 1 # Assign first observation as 1
-  set.seed(0) # Calculate various scenarios of Stock Performance
+  for (m in 1:ncol(P)){ c <- P[,m]
+    
+    r <- as.numeric(c / lag(c)) # Calculate returns
+    r[1] <- 1 # Assign first observation as 1
+    set.seed(0) # Calculate various scenarios of Stock Performance
+    
+    # Mimic Historical Performance using log returns
+    p <- data.table(apply(replicate(n,expr=round(sample(r,ndays,replace=T),
+                                                 2)),2,cumprod))
+    p$days <- 1:nrow(p)
+    p <- melt(p, id.vars = "days")
+    
+    # Make Line Charts with all scenarious
+    plt <- ggplot(p, aes(x=days,y=(value - 1) * 100, col=variable)) +
+      geom_line() +
+      theme_bw() +
+      theme(legend.position = "none") +
+      ggtitle(sprintf("%s Performance by Monte Carlo Simulation",
+                      colnames(c))) +
+      xlab("Days Invested") + 
+      ylab("Return (%)")
+    
+    summary <- as.vector(summary((p$value[p$days == ndays] - 1) * 100)) 
+    
+    names(summary) <- c("Min.", "1st Qu.", "Median", "Mean", "3rd Qu.", "Max.")
+    
+    L <- rbind(L, t(as.data.frame(summary))) # Join
+    
+    Av <- c(Av, as.vector(mean((p$value[p$days] - 1) * 100 < 0))) # Join
+    
+    if (is.null(Plots)){ Plots <- plt } else { Plots <- list(Plots, plt) } }
+    
+  names(Av) <- x # Assign names
   
-  # Mimic Historical Performance using log returns
-  p <- data.table(apply(replicate(n,expr=round(sample(r,ndays,replace=T),
-                                               2)),2,cumprod))
-  p$days <- 1:nrow(p)
-  p <- melt(p, id.vars = "days")
+  rownames(L) <- x # Assign row names
   
-  # Make Line Charts with all scenarious
-  monte_graph <- ggplot(p, aes(x=days,y=(value - 1) * 100, col=variable)) +
-    geom_line() +
-    theme_bw() +
-    theme(legend.position = "none") +
-    ggtitle(sprintf("%s Performance by Monte Carlo Simulation", colnames(c))) +
-    xlab("Days Invested") + 
-    ylab("Return (%)")
-  
-  monte_summary <- summary((p$value[p$days == ndays] - 1) * 100) # Stats
-  
-  monte_mean <- mean((p$value[p$days] - 1) * 100 < 0) # Expected Return
-  
-  list(monte_graph, monte_summary, monte_mean) # plot & stats
-}
-monte.carlo("GOOGL", 1000, 100) # Test
+  list(Plots, L, Av) # Output
+}    
+monte.carlo(c("GOOGL", "AMZN"), 1000, 100) # Test
