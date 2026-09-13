43db848a55fb7f2f6eee02049d714f3a7ee69c86
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-09-13 12:58:41 +0500

    Refactor Monte Carlo simulation code structure
    Refactor Monte Carlo simulation function for improved readability and performance. Adjusted data handling and plotting logic for better clarity.

--- Monte Carlo Simulation/Monte Carlo Method.R
+++ Monte Carlo Simulation/Monte Carlo Method.R
@@ -1,41 +1,57 @@
-lapply(c("quantmod", "ggplot2", "data.table", "timeSeries"),
-       require, character.only = T) # Libraries
+lapply(
+  c("quantmod", "ggplot2", "data.table", "timeSeries"), 
+  require, character.only = T
+  ) # Libraries
 
 monte.carlo <- function(x, ndays, n, yahoo = T){ # Monte Carlo Simulation
-
+  
   if (yahoo){ P <- NULL # When Data from Yahoo! Finance needed
-  
-    for (A in x){ P <- cbind(P, getSymbols(A,src="yahoo",auto.assign=F)[,4]) }
     
-    P <- P[apply(P, 1, function(x) all(!is.na(x))),] # Reduce NA
+    for (A in 1:length(x)){ s = getSymbols(x[A],src="yahoo",auto.assign=F)[,4] 
+        
+      message(
+        sprintf(
+          "%s is downloaded; %s from %s", 
+          x[A], which(x == x[A]), length(x)
+        )
+      )
     
-    colnames(P) <- x } else { P <- x } # Assign columns
+      s <- s[apply(s, 1, function(x) all(!is.na(x))),] # Reduce NA
+      
+      colnames(s) <- x[A] 
+      
+      if (!is.timeSeries(s)){ s <- as.timeSeries(s) }
+      
+      if (is.null(P)) P <- list(s) else P[[A]] <- s } }
     
-  if (!is.timeSeries(P)){ P <- as.timeSeries(P) }
-  
   L <- NULL
   Av <- NULL
   Plots <- NULL
   
-  for (m in 1:ncol(P)){ c <- P[,m]
+  for (m in 1:length(P)){ c <- P[[m]]
     
     r <- as.numeric(c / lag(c)) # Calculate returns
     r[1] <- 1 # Assign first observation as 1
     set.seed(0) # Calculate various scenarios of Stock Performance
     
     # Mimic Historical Performance using log returns
-    p <- data.table(apply(replicate(n,expr=round(sample(r,ndays,replace=T),
-                                                 2)),2,cumprod))
+    p <- data.table(
+      apply(
+        replicate(n, expr = round(sample(r, ndays, replace=T), 2)),
+        2,
+        cumprod
+        )
+      )
+    
     p$days <- 1:nrow(p)
     p <- melt(p, id.vars = "days")
     
-    # Make Line Charts with all scenarious
+    # Make Line Charts with all scenarios
     plt <- ggplot(p, aes(x=days,y=(value - 1) * 100, col=variable)) +
       geom_line() +
       theme_bw() +
       theme(legend.position = "none") +
-      ggtitle(sprintf("%s Performance by Monte Carlo Simulation",
-                      colnames(c))) +
+      ggtitle(sprintf("%s Monte Carlo Simulation Performance", colnames(c))) +
       xlab("Days Invested") + 
       ylab("Return (%)")
     
@@ -47,12 +63,21 @@ monte.carlo <- function(x, ndays, n, yahoo = T){ # Monte Carlo Simulation
     
     Av <- c(Av, as.vector(mean((p$value[p$days] - 1) * 100 < 0))) # Join
     
-    if (is.null(Plots)){ Plots <- plt } else { Plots <- list(Plots, plt) } }
+    if (is.null(Plots)) Plots <- list(plt) else Plots[[m]] <- plt }
     
-  names(Av) <- colnames(P) # Assign names
+  names(Plots) <- x
+  names(Av) <- x # Assign names
+  rownames(L) <- x # Assign row names
   
-  rownames(L) <- colnames(P) # Assign row names
+  L <- as.data.frame(L)
   
-  list(Plots, L, Av) # Output
+  L <- L[order(-L$`Median`), ] # Sort by yield level
+  Av <- sort(Av, decreasing = F)
+  
+  DF <- list(Plots, L, Av) # Output
+  
+  names(DF) <- c("Plots", "Yield", "Means")
+  
+  DF
 }    
 monte.carlo(c("GOOGL", "AMZN"), 1000, 100) # Test
