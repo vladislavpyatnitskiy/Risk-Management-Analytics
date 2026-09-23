79a9d58a15f2d0aade78c45d5fe6b400e02f1fad
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-09-23 13:07:32 +0500

    Enhance monte.carlo.es function for CVaR analysis
    Refactor Monte Carlo simulation function for Expected Shortfall to include Yahoo Finance data retrieval and improve return calculations.

--- Expected Shortfall (CVaR)/CVaR via Monte.R
+++ Expected Shortfall (CVaR)/CVaR via Monte.R
@@ -1,32 +1,62 @@
-lapply(c("quantmod", "ggplot2", "data.table", "timeSeries"), # Libraries
-       require, character.only = TRUE)
+lapply(c("quantmod", "ggplot2", "data.table", "timeSeries"), 
+       require, character.only = TRUE) # Libraries
 
 # Monte Function for Expected Shortfall
-monte.carlo.es <- function(c, ndays, n, ES = 95){ l <- NULL
+monte.carlo.es <- function(x, ndays, n, ES = 95, yahoo=T){ 
   
-  # For each column in data set
-  for (b in 1:ncol(c)){ v <- as.numeric(c[,b] / lag(c[,b])) # Calculate return
+  if (yahoo){ P <- NULL # When Data from Yahoo! Finance needed
     
-    v[1] <- 1 # Define first value in column as 1
+    for (A in 1:length(x)){ s = getSymbols(x[A],src="yahoo",auto.assign=F)[,4] 
     
+    message(
+      sprintf(
+        "%s is downloaded; %s from %s", 
+        x[A], which(x == x[A]), length(x)
+      )
+    )
+    
+    s <- s[apply(s, 1, function(x) all(!is.na(x))),] # Reduce NA
+    
+    colnames(s) <- x[A] 
+    
+    if (!is.timeSeries(s)){ s <- as.timeSeries(s) }
+    
+    if (is.null(P)) P <- list(s) else P[[A]] <- s } }
+    
+  L <- NULL
+
+  for (m in 1:length(P)){ c <- P[[m]]
+
+    r <- as.numeric(c / lag(c)) # Calculate returns
+    r[1] <- 1 # Assign first observation as 1
     set.seed(0) # Calculate various scenarios of Stock Performance
-    
-    # Mimic Historical Performance & calculate cumulative sums
-    p <- apply(replicate(n,expr=round(sample(v,ndays,replace=T),2)),2,cumprod)
-    
-    # Transform it into Time Series
-    p <- data.table(p)
+
+    # Mimic Historical Performance using log returns
+    p <- data.table(
+      apply(
+        replicate(n, expr = round(sample(r, ndays, replace=T), 2)),
+        2,
+        cumprod
+      )
+    )
+
     p$days <- 1:nrow(p)
     p <- melt(p, id.vars = "days")
-    
-    # Calculate CVaR and add to list
-    l <- rbind(l, mean(quantile(((p$value[p$days == ndays] - 1) * 100),
-                                probs = seq(0, 1 - CVaR * .01, 1/n)/ndays))) }
-  
-  rownames(l) <- colnames(c) # Give row name
-  colnames(l) <- "ES MC" # Give column name
-  
-  return(l) # Display values
+
+    L <- rbind(
+      L,
+      mean(
+        quantile(
+          (p$value[p$days == ndays] - 1) * 100,
+          probs = seq(0, 1 - ES * .01, 1 / n) / ndays
+          )
+        )
+      )
+  }
+
+  rownames(L) <- x
+  colnames(L) <- "CVaR MC (%)"
+
+  round(L, 2) # Display values
 }
-# Test
-monte.carlo.es(portfolioReturns, ndays = 252, n = 100)
+monte.carlo.es(c("GOOGL", "AMZN"), 250, 100) # Test
