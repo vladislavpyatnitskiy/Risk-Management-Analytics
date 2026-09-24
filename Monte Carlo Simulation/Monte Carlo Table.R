ae822d6466b280e473848deca2e6d08b65eb5b20
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-09-24 13:09:33 +0500

    Enhance monte.carlo.table function structure
    Refactor monte.carlo.table function to improve parameter handling and data processing.

--- Monte Carlo Simulation/Monte Carlo Table.R
+++ Monte Carlo Simulation/Monte Carlo Table.R
@@ -1,33 +1,57 @@
 lapply(c("quantmod", "ggplot2", "data.table", "timeSeries"),
        require, character.only = T) # Libraries
 
-monte.carlo.table <- function(tickers, ndays, n){ # Table for Monte values
+monte.carlo.table <- function(x, ndays, n){ # Table for Monte values
   
-  p <- NULL # Assign empty variable
+  P <- NULL # When Data from Yahoo! Finance needed
   
-  for (a in tickers){ p<-cbind(p,getSymbols(a,src="yahoo",auto.assign=F)[,4]) }
+  for (A in 1:length(x)){ s = getSymbols(x[A],src="yahoo",auto.assign=F)[,4] 
+    
+    message(
+      sprintf(
+        "%s is downloaded; %s from %s", 
+        x[A], which(x == x[A]), length(x)
+      )
+    )
+    
+    s <- s[apply(s, 1, function(x) all(!is.na(x))),] # Reduce NA
+    
+    colnames(s) <- x[A] 
+    
+    if (!is.timeSeries(s)){ s <- as.timeSeries(s) }
+    
+    if (is.null(P)) P <- list(s) else P[[A]] <- s }
+    
+  L <- NULL
   
-  p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Reduce rows with NA
-  
-  colnames(p) <- tickers # Assign tickers and calculate returns
-  
-  l <- NULL # Empty variable & calculate returns
-  
-  for (m in 1:ncol(p)){ r <- as.numeric(diff(log(as.timeSeries(p[,m]))) + 1)
-  
+  for (m in 1:length(P)){ c <- P[[m]]
+    
+    r <- as.numeric(c / lag(c)) # Calculate returns
     r[1] <- 1 # Assign first observation as 1
     set.seed(0) # Calculate various scenarios of Stock Performance
     
     # Mimic Historical Performance using log returns
-    v <- data.table(apply(replicate(n, expr=round(sample(r, ndays, replace=T),
-                                                  2)), 2, cumprod))
-    v$days <- 1:nrow(v)
-    v <- melt(v, id.vars = "days") # Add to matrix
+    p <- data.table(
+      apply(
+        replicate(n, expr = round(sample(r, ndays, replace=T), 2)),
+        2,
+        cumprod
+      )
+    )
     
-    l <- rbind(l, t(as.matrix(summary((v$value[v$days == ndays] - 1)*100)))) }
+    p$days <- 1:nrow(p)
+    p <- melt(p, id.vars = "days")
+    
+    summary <- as.vector(summary((p$value[p$days == ndays] - 1) * 100)) 
+    
+    names(summary) <- c("Min.", "1st Qu.", "Median", "Mean", "3rd Qu.", "Max.")
+    
+    L <- rbind(L, t(as.data.frame(summary))) } # Join
+    
+  rownames(L) <- x # Assign row names
   
-  rownames(l) <- tickers # Assign tickers for row names
+  L <- as.data.frame(L)
   
-  l # Display
+  L[order(-L$`Median`), ] # Sort by yield level
 }
 monte.carlo.table(c("UNM", "MET", "AIG", "OMF", "HIG"), 1000, 100) # Test
